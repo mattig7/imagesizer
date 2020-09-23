8235981aa40d0f14864d7bd965b40cebf22201c2
Matt Graham <matt_graham2001@hotmail.com>
2020-09-23 21:09:42 +1000

    Completed convertImages function
    Completed first version of convertImages function, and it appears to be
    working correctly.

--- imagesizer.py
+++ imagesizer.py
@@ -46,7 +46,7 @@ parser = argparse.ArgumentParser(description='Find all images in a directory and
 
 parser.add_argument('--source_dir', help='The directory to search for images', default=Path.cwd())
 parser.add_argument('--dest_dir', help='The directory to put all of the resized images', default=Path.cwd()/Path('ResizedImages/'))
-parser.add_argument('-w', '--width', help='The width in pixels to make the new images. Aspect ratio is always maintained', type=int, metavar='W', default=750)
+parser.add_argument('-w', '--width', help='The width in pixels to make the new images. Aspect ratio is always maintained', type=int, metavar='W', default=720)
 parser.add_argument('-r', '--recursive', help='Whether to recursively search sub-directories from the main source directory or not', action='store_const', const=True, default=False)
 
 args = parser.parse_args()
@@ -55,12 +55,60 @@ logger.debug(f"Established arguments passed in as:\n{str(args)}")
 
 logger.debug("Image Sizer setup complete.")
 
+def isImageFilename(filename):
+    """
+    checks to see whether a filename is a valid image file type (extension).
+    Returns true if it is, false otherwise.
+    """
+    logger.debug(f"Commencing function 'isImageFilename' with filename {filename}")
+    
+    listOfImageExtensions = [".jpg", ".png", ".tiff", ".bmp", ".gif"]
+
+    filename = getPath(filename)
+
+    if filename.suffix.lower() in listOfImageExtensions:
+        logger.debug(f"Found the image extension {filename} within list of Image extensions {str(listOfImageExtensions)}. Returning True.")
+        return True
+    logger.debug(f"Did not find the extension of {filename} within the list of Image extensions {str(listOfImageExtensions)}. Returning False.")
+    return False
+
+def getPath(filePath):
+    """
+    Function to check to see if a parameter is actually a file path, and attempt to convert it if it isn't. Returns the object passed in if it is a path, and a path object otherwise (if it can).
+    """
+    logger.debug(f"Started function 'getPath' with file {filePath} whose type is {type(filePath)}") 
+    if type(filePath) != type(Path.cwd()):
+        logger.debug(f"User didn't pass a Path object. Attempting to correct this")
+        filePath = Path(filePath)
+    logger.debug(f"Returning object whose type is {type(filePath)}.")
+    return filePath
+
+def convertImage(imagePath, destDir=Path.cwd(), width=720):
+    """
+    Function to convert a single image to a new size (based on its width).
+    If not provided, the new width becomes the default of 720.
+    """
+    logger.debug(f"Commenced function 'convertImage' with:\nimage path:{imagePath}\ndestinationDirectory: {destDir}\nWidth: {width}")
+    
+    #ensure we are working with Path objects
+    imagePath = getPath(imagePath)
+    destDir = getPath(destDir)
+
+    #TODO create a new image object based on the current imagefile
+    
+    #TODO Call function to resize image object
+    
+    #TODO Save new image in destination dir
+    
+    logger.debug("Completed convertImage function")
+
+
 
 def convertDir(newWidth, source_Directory, dest_Directory=None, recursive=False):
     """
     Goes through a directory converting all images found into a new image with the new width.
     Steps through all files within the directory path. 
-    If they are images, it will copy them to the destinaton directory and resize them with the new width (maintaining aspect ratio).
+    If they are images, it will call 'convertImage' function on them.
     """
     logger.debug(f"Commenced convertImagesInDir function with:\nnewWidth: {newWidth}\nsource_Directory: {source_Directory}\ndestination_Directory: {dest_Directory}\nrecursive: {recursive}")
     
@@ -68,15 +116,22 @@ def convertDir(newWidth, source_Directory, dest_Directory=None, recursive=False)
         logger.debug(f"No destination directory provided. Creating a new one off source directory")
         dest_Directory = source_Directory/Path('/convertedImages')
     
-    #TODO LOOK UP HOW TO DO A RECURSIVE GLOB!!!
     
-    for imagePath in source_Directory.glob('*'):
-        logger.debug(f"Looking through image files. Up to file {imagePath}")
-        #TODO create a new image object based on the current imagefile
+    for thisFolder, subFolders, filenames in os.walk(source_Directory):
+        logger.debug(f"Walking through {source_Directory}. This folder is {thisFolder}. Sub-folders are \n{str(subFolders)}\nfilenames are {str(filenames)}")
         
-        #TODO Call function to resize image object
-    
-        #TODO Save new image in destination dir
+        if not recursive:
+            logger.debug(f"Since this call is not recursive, removing subFolders {str(subFolders)}")
+            subFolders.clear()
+            logger.debug(f"The subfolders list is now {str(subFolders)}.")
+        
+        for filePath in filenames:
+            logger.debug(f"Checking to see whether {filePath} has an image extension")
+            if isImageFilename(filePath):
+                logger.debug(f"The file {filePath} is an image file.")
+                convertImage(os.path.join(thisFolder, filePath), dest_Directory, newWidth)
+                
+        logger.debug(f"Completed looking at the files in directory '{thisFolder}'. Moving to next")
     
     logger.debug("Completed function convertImagesInDir")
 
